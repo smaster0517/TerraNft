90f329249a3048e6eadd8638b7b361ae15658d47
smaster0517 <smaster0517@gmail.com>
2021-12-05 17:56:26 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+pub mod msg;
+
+use cw721_metadata_template::{Cw721MetadataContract, Metadata};
+
+pub use cw721_base::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
+
+use crate::msg::ExecuteMsg;
+
+type Cw721ExecuteMsg = cw721_metadata_template::ExecuteMsg;
+
+#[cfg(not(feature = "library"))]
+pub mod entry {
+    use super::*;
+
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+
+    // This is a simple type to let us handle empty extensions
+
+    // This makes a conscious choice on the various generics used by the contract
+    #[entry_point]
+    pub fn instantiate(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: InstantiateMsg,
+    ) -> StdResult<Response> {
+        // this is custom, taking our custom instantiate message.
+        Cw721MetadataContract::default().instantiate(deps, env, info, msg)
+    }
+
+    #[entry_point]
+    pub fn execute(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        match msg {
+            ExecuteMsg::StubMsg{token_id, token_uri, owner_id, attributes} => {
+								// do some custom handling....
+						  	Ok(Response::default());
+				  	},
+            _ => Cw721MetadataContract::default().execute(deps, env, info, Cw721ExecuteMsg::from(msg)),
+        }
+    }
+
+    #[entry_point]
+    pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+        // This needs to handle static stub on NftInfo and AllNftInfo
+        Cw721MetadataContract::default().query(deps, env, msg)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cw721::Cw721Query;
+
+    const CREATOR: &str = "creator";
+
+    #[test]
+    fn use_metadata_extension() {
+        let mut deps = mock_dependencies();
+        let contract = Cw721MetadataContract::default();
+
+        let info = mock_info(CREATOR, &[]);
+        let init_msg = InstantiateMsg {
+            name: "SpaceShips".to_string(),
+            symbol: "SPACE".to_string(),
+            minter: CREATOR.to_string(),
+        };
+        contract
+            .instantiate(deps.as_mut(), mock_env(), info.clone(), init_msg)
+            .unwrap();
+
+        let token_id = "Enterprise";
+        let mint_msg = MintMsg {
+            token_id: token_id.to_string(),
+            owner: "john".to_string(),
+            token_uri: Some("https://starships.example.com/Starship/Enterprise.json".into()),
+            extension: Some(Metadata {
+                description: Some("Spaceship with Warp Drive".into()),
+                name: Some("Starship USS Enterprise".to_string()),
+                ..Metadata::default()
+            }),
+        };
+        let exec_msg = cw721_metadata_template::ExecuteMsg::Mint(mint_msg.clone());
+        contract
+            .execute(deps.as_mut(), mock_env(), info, exec_msg)
+            .unwrap();
+
+        let res = contract.nft_info(deps.as_ref(), token_id.into()).unwrap();
+        assert_eq!(res.token_uri, mint_msg.token_uri);
+        assert_eq!(res.extension, mint_msg.extension);
+    }
+}
---
+++ src/msg.rs
@@ -0,0 +1,88 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Binary;
+
+use cw721::Expiration;
+use cw721_base::MintMsg;
+use cw721_metadata_template::Extension;
+
+type Cw721ExecuteMsg = cw721_metadata_template::ExecuteMsg;
+
+/// Have to copy Msg variants from base, as 
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub enum ExecuteMsg {
+    StubMsg {
+        token_id: String,
+        token_uri: String,
+        owner_id: String,
+        attributes: String,
+    },
+
+    Mint(MintMsg<Extension>),
+    TransferNft {
+        recipient: String,
+        token_id: String,
+    },
+    SendNft {
+        contract: String,
+        token_id: String,
+        msg: Binary,
+    },
+    Approve {
+        spender: String,
+        token_id: String,
+        expires: Option<Expiration>,
+    },
+    Revoke {
+        spender: String,
+        token_id: String,
+    },
+    ApproveAll {
+        operator: String,
+        expires: Option<Expiration>,
+    },
+    RevokeAll {
+        operator: String,
+    },
+}
+
+impl From<ExecuteMsg> for Cw721ExecuteMsg {
+    fn from(msg: ExecuteMsg) -> Cw721ExecuteMsg {
+        match msg {
+            ExecuteMsg::TransferNft {
+                recipient,
+                token_id,
+            } => Cw721ExecuteMsg::TransferNft {
+                recipient,
+                token_id,
+            },
+            ExecuteMsg::SendNft {
+                contract,
+                token_id,
+                msg,
+            } => Cw721ExecuteMsg::SendNft {
+                contract,
+                token_id,
+                msg,
+            },
+            ExecuteMsg::Approve {
+                spender,
+                token_id,
+                expires,
+            } => Cw721ExecuteMsg::Approve {
+                spender,
+                token_id,
+                expires,
+            },
+            ExecuteMsg::Revoke { spender, token_id } => {
+                Cw721ExecuteMsg::Revoke { spender, token_id }
+            }
+            ExecuteMsg::ApproveAll { operator, expires } => {
+                Cw721ExecuteMsg::ApproveAll { operator, expires }
+            }
+            ExecuteMsg::RevokeAll { operator } => Cw721ExecuteMsg::RevokeAll { operator },
+            _ => panic!("cannot convert {:?} to Cw721ExecuteMsg", msg),
+        }
+    }
+}

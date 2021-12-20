f6421cbd87d35c0203dccadf963b7620e23c96f6
smaster0517 <smaster0517@gmail.com>
2021-12-20 17:54:55 +0100

    Finish lifting and adapting usage example

--- tooling/src/main.rs
+++ tooling/src/main.rs
@@ -1,13 +1,15 @@
+use std::path::Path;
 use anyhow::Error;
+use terra_rust_api::messages::wasm::MsgStoreCode;
 use terra_rust_api::{Terra, GasOptions, PrivateKey};
-use terra_rust_api::core_types::{Coin, StdSignMsg, StdSignature};
-use terra_rust_api::messages::{MsgSend, Message};
+use terra_rust_api::messages::Message;
 use bitcoin::secp256k1::Secp256k1;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, contract!");
 
-    let result = doTheThings();
+    let result = do_the_things().await;
 
     match result {
         Ok(msg) =>  println!("All done! ({}", msg),
@@ -15,12 +17,43 @@ fn main() {
     }
 }
 
-fn doTheThings() -> Result<String, Error> {
+async fn do_the_things() -> Result<String, Error> {
     // To start, just give relative file path
     // Initialize client
     let gas_opts = GasOptions::create_with_gas_estimate("50ukrw",1.4)?;
     let terra = Terra::lcd_client("https://localhost:1417/", "localterra", &gas_opts,None);
 
+    let secp = Secp256k1::new();
+    let from_key = PrivateKey::from_words(&secp,"notice oak worry limit wrap speak medal online prefer cluster roof addict wrist behave treat actual wasp year salad speed social layer crew genius",0,0)?;
+
+    // test1 on localterra
+    let sender = "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v";
+    let wasm_file = Path::new("../artifacts/terra_nft_stub.wasm");
+
+    let msg = MsgStoreCode::create_from_file(sender, wasm_file)?;
+
+    let messages: Vec<Message> = vec![msg];
+    let (std_sign_msg, sigs) = terra
+       .generate_transaction_to_broadcast(
+           &secp,
+           &from_key,
+           messages,
+           None
+       )
+       .await?;
+
+    // send it out
+    let resp = terra.tx().broadcast_sync(&std_sign_msg, &sigs).await?;
+
+    match resp.code {
+        Some(code) => {
+            log::error!("{}", serde_json::to_string(&resp)?);
+            eprintln!("Transaction returned a {} {}", code, resp.txhash)
+        }
+        None => {
+            println!("{}", resp.txhash)
+        }
+    }
     // My features
     // 1. Build message
     // 2. Send
